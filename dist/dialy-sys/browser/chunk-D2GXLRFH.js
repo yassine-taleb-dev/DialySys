f0f0624aadf85d2e38d93c36c326fb964a3a7307
nouhaila2001204 <blachenouhaila@gmail.com>
2026-05-05 23:47:58 +0100

    version mobile

---
+++ dist/dialy-sys/browser/chunk-D2GXLRFH.js
@@ -0,0 +1,1 @@
+import{La as o,Ma as a,X as n,Za as i,hb as r}from"./chunk-V22EUVVG.js";var p=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-unauthorized"]],standalone:!0,features:[r],decls:2,vars:0,template:function(e,s){e&1&&(o(0,"p"),i(1,"unauthorized works!"),a())}})};export{p as UnauthorizedComponent};
